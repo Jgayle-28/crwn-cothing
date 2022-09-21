8651efc77692e329b8065181694b8fd1a28bbd42
Jerehme Gayle <jerehme.gayle@gmail.com>
2022-09-20 20:22:43 -0700

    Home page and styles complete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
+import Home from "./pages/home/Home"
+
 function App() {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img src="" alt="category" /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img src="" alt="category" /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img src="" alt="category" /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img src="" alt="category" /> */}
-        <div className='category-body-container'>
-          <h2>Womans</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img src="" alt="category" /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  )
+  return <Home />
 }
 
 export default App
