61552424be440d4a05e36b2a2aa47e8604699759
Jerehme Gayle <jerehme.gayle@gmail.com>
2022-09-24 08:26:38 -0700

    Create useClickAway hook and added click away listener to cart dropdown in nav bar

--- src/components/shop/cart-dropdown/CartDropdown.component.jsx
+++ src/components/shop/cart-dropdown/CartDropdown.component.jsx
@@ -1,18 +1,23 @@
-import { useContext } from "react"
+import { useContext, useRef } from "react"
 import { ReactComponent as BagIcon } from "assets/shopping-bag.svg"
+import { useClickAway } from "hooks/useClickAway"
+import { ShopContext } from "context/shop/Shop.context"
 import { Button } from "components/theme"
+import CartDropdownItem from "../cart-dropdown-item/CartDropdownItem"
 import "./cart-dropdown.styles.scss"
 import "./cart-icon.styles.scss"
-import { ShopContext } from "context/shop/Shop.context"
-import CartDropdownItem from "../cart-dropdown-item/CartDropdownItem"
 
 function CartDropdown() {
   const { cartItems, cartCount, cartMenuOpen, setCartMenuOpen } =
     useContext(ShopContext)
 
+  const cartDropdownRef = useRef()
+  useClickAway(cartDropdownRef, () => setCartMenuOpen(!cartMenuOpen))
+
   return (
     <>
       <div
+        ref={cartDropdownRef}
         className='cart-icon-container'
         onClick={() => setCartMenuOpen(!cartMenuOpen)}
       >
